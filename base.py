c143bc92ca2bf7177933d5f2a569f16103cafb3a
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2017-10-18 15:51:25 +0200

    context processor for entities missing

--- base.py
+++ base.py
@@ -120,6 +120,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'webpage.context_processors.custom_context_processors.add_entities'
             ],
         },
     },
